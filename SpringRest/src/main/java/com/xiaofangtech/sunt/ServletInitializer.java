9c990fde92762b0b0d12ede1447502f8fdb36d3d
wuhlcom <378433855@qq.com>
2017-07-18 10:42:41 +0800

    zhilu TblIotDevice add

--- SpringRest/src/main/java/com/xiaofangtech/sunt/ServletInitializer.java
+++ SpringRest/src/main/java/com/xiaofangtech/sunt/ServletInitializer.java
@@ -1,13 +1,13 @@
-package com.xiaofangtech.sunt;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringRestApplication.class);
-	}
-
-}
+//package com.xiaofangtech.sunt;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.context.web.SpringBootServletInitializer;
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(SpringRestApplication.class);
+//	}
+//
+//}
---
+++ javatest/src/object/Test1.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class Test1 {
+	public static void main(String[] args) {
+	 int protocol = 1;
+     if (protocol == 0 || protocol == 1 || protocol == 2 || protocol == 3) {
+		System.out.println("aaaaaaaaa");
+	 }
+	}
+}
